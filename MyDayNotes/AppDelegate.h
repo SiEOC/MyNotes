cbaa5363b83bcfc3a9037a64ca38f7472c2c5587
Skyler Clark <skyler@whitecapinstitute.com>
2015-07-15 06:16:01 -0600

    Initial Commit

---
+++ MyDayNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDayNotes
+//
+//  Created by Skyler Clark on 7/15/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
